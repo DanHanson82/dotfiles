82cecb28104fc7bc5b3599c5ed291bd114fec30b
Daniel Hanson <daniel.hanson@sendoutcards.com>
2016-06-09 14:02:07 -0600

    fixed nvim config for osx

--- bootstrap.sh
+++ bootstrap.sh
@@ -39,4 +39,8 @@ for file in $files; do
     ln -s $dir/$file ~/$file
 done
 
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+
 vim +PluginInstall +qall
