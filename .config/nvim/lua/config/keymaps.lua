4f36cc3a31defde8fb55c1a3312184ae9257820a
Dan Hanson <dan.hanson82@gmail.com>
2023-01-13 16:03:08 -0700

    cleanup

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,67 +9,67 @@ vim.g.maplocalleader = ","
 local set = vim.keymap.set
 
 -- lsp
-vim.keymap.set('n', '<leader>lt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-vim.keymap.set('n', '<leader>lf', '<cmd>:lua vim.lsp.buf.formatting()<CR>')
-vim.keymap.set('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<CR>')
-vim.keymap.set('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<CR>')
-vim.keymap.set('n', '<leader>lh ', '<cmd>lua vim.lsp.buf.hover()<CR>')
+set('n', '<leader>lt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+set('n', '<leader>lf', '<cmd>:lua vim.lsp.buf.formatting()<CR>')
+set('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<CR>')
+set('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<CR>')
+set('n', '<leader>lh ', '<cmd>lua vim.lsp.buf.hover()<CR>')
 -- method textDocument/rename is not supported by any of the servers registered for the current buffer
-vim.keymap.set('n', '<silent>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+set('n', '<silent>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
 
 -- trouble
-vim.keymap.set('n', '<leader>xx', '<cmd>TroubleToggle<cr>')
-vim.keymap.set('n', '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>')
-vim.keymap.set('n', '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>')
-vim.keymap.set('n', '<leader>xq', '<cmd>TroubleToggle quickfix<cr>')
-vim.keymap.set('n', '<leader>xl', '<cmd>TroubleToggle loclist<cr>')
+set('n', '<leader>xx', '<cmd>TroubleToggle<cr>')
+set('n', '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>')
+set('n', '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>')
+set('n', '<leader>xq', '<cmd>TroubleToggle quickfix<cr>')
+set('n', '<leader>xl', '<cmd>TroubleToggle loclist<cr>')
 -- nnoremap gR <cmd>TroubleToggle lsp_references<cr>
 
 -- packer commands
-vim.keymap.set('n', '<leader>pi', ':PackerInstall<Return>')
-vim.keymap.set('n', '<leader>pu', ':PackerUpdate<Return>')
-vim.keymap.set('n', '<leader>ps', ':PackerSync<Return>')
-vim.keymap.set('n', '<leader>pc', ':PackerClean<Return>')
+set('n', '<leader>pi', ':PackerInstall<Return>')
+set('n', '<leader>pu', ':PackerUpdate<Return>')
+set('n', '<leader>ps', ':PackerSync<Return>')
+set('n', '<leader>pc', ':PackerClean<Return>')
 
 -- Find files using Telescope
 --nnoremap <leader>ff <cmd>Telescope find_files<cr>
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files<cr>')
-vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
-vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
-vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
+set('n', '<leader>ff', '<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files<cr>')
+set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
+set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 
 -- Treesitter
-vim.keymap.set('n', '<leader>tsp', ':TSPlaygroundToggle<Return>')
+set('n', '<leader>tsp', ':TSPlaygroundToggle<Return>')
 
 -- Fugitive git commands
 -- changing prefix to g for git instead of fugitive since f clashes with find
 -- or file
-vim.keymap.set('n', '<leader>gd', ':Gvdiff<Return>')
-vim.keymap.set('n', '<leader>gdu', ':diffupdate<Return>')
-vim.keymap.set('n', '<leader>gdg', ':diffget<Return>')
-vim.keymap.set('n', '<leader>gdp', ':diffput<Return>')
-vim.keymap.set('n', '<leader>gw', ':Gwrite<Return>')
-vim.keymap.set('n', '<leader>gwi', ':Gwrite!<Return>')
-vim.keymap.set('n', '<leader>gr', ':Gread<Return>')
-vim.keymap.set('n', '<leader>gb', ':Git blame<Return>')
-vim.keymap.set('n', '<leader>gs', ':Git<Return>')
+set('n', '<leader>gd', ':Gvdiff<Return>')
+set('n', '<leader>gdu', ':diffupdate<Return>')
+set('n', '<leader>gdg', ':diffget<Return>')
+set('n', '<leader>gdp', ':diffput<Return>')
+set('n', '<leader>gw', ':Gwrite<Return>')
+set('n', '<leader>gwi', ':Gwrite!<Return>')
+set('n', '<leader>gr', ':Gread<Return>')
+set('n', '<leader>gb', ':Git blame<Return>')
+set('n', '<leader>gs', ':Git<Return>')
 
 -- mix format
-vim.keymap.set('n', '<leader>mf', ':MixFormat<Return>')
+set('n', '<leader>mf', ':MixFormat<Return>')
 
 -- json pretty print
-vim.keymap.set('n', '<leader>jq', ':%!jq<Return>')
+set('n', '<leader>jq', ':%!jq<Return>')
 
 -- window commands
-vim.keymap.set('n', '<leader>%', ':vs.<Return>')
-vim.keymap.set('n', '<leader>"', ':sp.<Return>')
-vim.keymap.set('n', '<leader>o', ':only<Return>')
+set('n', '<leader>%', ':vs.<Return>')
+set('n', '<leader>"', ':sp.<Return>')
+set('n', '<leader>o', ':only<Return>')
 
 -- new tab
-vim.keymap.set('n', '<leader>tn', ':tabe<Return>')
+set('n', '<leader>tn', ':tabe<Return>')
 
 -- pure laziness
-vim.keymap.set('n', '<leader>q', ':q<Return>')
-vim.keymap.set('n', '<leader>qa', ':qa<Return>')
-vim.keymap.set('n', '<leader>w', ':w<Return>')
-vim.keymap.set('n', '<leader>wq', ':wq<Return>')
+set('n', '<leader>q', ':q<Return>')
+set('n', '<leader>qa', ':qa<Return>')
+set('n', '<leader>w', ':w<Return>')
+set('n', '<leader>wq', ':wq<Return>')
